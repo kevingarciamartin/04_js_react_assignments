1812a9cc99faab2e6c49603275be4744afaa4493
Kevin Garcia Martin <kevingm@live.se>
2025-01-08 16:10:11 +0100

    TODO 1

--- 5/script.js
+++ 5/script.js
@@ -6,13 +6,16 @@ const apiUrl = "https://api.example.com/data";
 function fetchData(url) {
   fetch(url)
     .then((response) => {
-      if (response.ok) {
+      if (!response.ok) {
         throw new Error("Network response was not ok " + response.statusText);
       }
       return response.json();
     })
     .then((data) => {
       console.log(data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
     });
 }
 
