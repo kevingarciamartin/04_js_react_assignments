4b0df06a5dc6a8f11ee6e4fbf5c94c4cba21e157
Kevin Garcia Martin <kevingm@live.se>
2025-01-15 16:11:09 +0100

    Assignment 8: Child Component

---
+++ react-assignments/src/ChildComponent.jsx
@@ -0,0 +1,15 @@
+function ChildComponent() {
+  return (
+    <>
+      <h2>This is a child component</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, libero
+        tempora maiores suscipit sed vitae similique, expedita accusantium
+        delectus quisquam laboriosam eveniet ab nam eos temporibus itaque,
+        placeat magni veniam.
+      </p>
+    </>
+  );
+}
+
+export default ChildComponent;
